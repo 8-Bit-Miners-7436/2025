62ba7fd181ef767ff857276e285d9f919812c7d2
8-Bit Miners <202030680+8-Bit-Miners@users.noreply.github.com>
2025-03-05 18:32:50 -0700

    General Cleanup (WIP)
    Started refactoring Robot.java and adding comments.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,49 +14,34 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.utils.CustomSwerveModule;
 
 public class Robot extends TimedRobot {
-  private XboxController gamepad;
-  private Pigeon2 pigeon;
-  private Translation2d frontLeftLocation;
-  private Translation2d frontRightLocation;
-  private Translation2d rearLeftLocation;
-  private Translation2d rearRightLocation;
+  // * Initializes Xbox Controller and IMU (Gyro)
+  private final XboxController gamepad = new XboxController(0);
+  private final Pigeon2 pigeon = new Pigeon2(10);
+  
+  // * Initializes Kinematics Object w/ Swerve Module Offsets From Robot Center (In Meters)
+  private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+    new Translation2d(0.34, 0.3), // front-left
+    new Translation2d(0.34, -0.3),  // front-right
+    new Translation2d(-0.34, 0.3),  // rear-left
+    new Translation2d(-0.34, -0.3)    // rear-right
+  );
+    
+  // * Initializes Swerve Modules And Package In Array For Easy Modular Access
+  private final CustomSwerveModule[] modules = new CustomSwerveModule[] {
+    new CustomSwerveModule(1), // front-left
+    new CustomSwerveModule(2), // front-right
+    new CustomSwerveModule(3), // rear-left
+    new CustomSwerveModule(4)  // rear-right
+  };
 
-  private SwerveDriveKinematics kinematics;
-  private SwerveModuleState[] moduleStates;
-  private CustomSwerveModule[] modules;
-
-  private double pigeonYaw;
-  private double pigeonPitch;
-  private double pigeonRoll;
-
-  private SparkMax elevatorMotor;
-
-  @Override
-  public void robotInit() {
-    gamepad = new XboxController(0);
-    pigeon = new Pigeon2(10);
-    frontLeftLocation = new Translation2d(0.34, 0.3);
-    frontRightLocation = new Translation2d(0.34, -0.3);
-    rearLeftLocation = new Translation2d(-0.34, 0.3);
-    rearRightLocation = new Translation2d(-0.34, -0.3);
-
-    kinematics = new SwerveDriveKinematics(frontLeftLocation, frontRightLocation, rearLeftLocation, rearRightLocation);
-
-    modules = new CustomSwerveModule[] {
-        new CustomSwerveModule(1), // front-left
-        new CustomSwerveModule(2), // front-right
-        new CustomSwerveModule(3), // rear-left
-        new CustomSwerveModule(4), // rear-right
-    };
-
-    elevatorMotor = new SparkMax(15, MotorType.kBrushless);
-  }
+  // Todo: Make a More Permanent Elevator Solution
+  private SparkMax elevatorMotor = new SparkMax(15, MotorType.kBrushless);
 
   @Override
   public void robotPeriodic() {
-    pigeonYaw = pigeon.getYaw().getValueAsDouble() % 360;
-    pigeonPitch = pigeon.getPitch().getValueAsDouble() % 360;
-    pigeonRoll = pigeon.getRoll().getValueAsDouble() % 360;
+    double pigeonYaw = pigeon.getYaw(true).getValueAsDouble() % 360;
+    double pigeonPitch = pigeon.getPitch(true).getValueAsDouble() % 360;
+    double pigeonRoll = pigeon.getRoll(true).getValueAsDouble() % 360;
     SmartDashboard.putNumber("Pigeon Yaw", pigeonYaw);
     SmartDashboard.putNumber("Pigeon Pitch", pigeonPitch);
     SmartDashboard.putNumber("Pigeon Roll", pigeonRoll);
@@ -66,11 +51,11 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     double vx = gamepad.getLeftY(); // * Forward/backward
     double vy = gamepad.getLeftX(); // * Strafing
-    double a = gamepad.getRightX(); // / Math.abs(pigeonYaw/360); // * Rotation
+    double a = gamepad.getRightX(); // * Rotation
 
     ChassisSpeeds chassisSpeeds = new ChassisSpeeds(vx, vy, a);
 
-    moduleStates = kinematics.toSwerveModuleStates(chassisSpeeds);
+    SwerveModuleState moduleStates[] = kinematics.toSwerveModuleStates(chassisSpeeds);
 
     for (int i = 0; i < modules.length; i++) {
       SwerveModuleState state = moduleStates[i];
@@ -82,6 +67,8 @@ public class Robot extends TimedRobot {
       modules[i].setDriveSpeed(targetSpeed);
       modules[i].setTargetAngle(targetAngle);
     }
+
+    // Todo: Make a More Permanent Elevator Solution
     if (gamepad.getAButton()) {
       elevatorMotor.set(1);
     } else if (gamepad.getBButton()) {
@@ -93,6 +80,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testInit() {
+    // * Resets Swerve Module Steer Motor Encoders
+    // * Start Test Mode Briefly After Re-Aligning Wheels Manually
     for (int i = 0; i < modules.length; i++) {
       modules[i].resetEncoder();
     }
